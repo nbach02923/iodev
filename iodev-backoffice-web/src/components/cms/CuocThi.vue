69b1530c3b7db356103ea9d2a6a5c06406716eaa
dangkhanhtrung <trung.dk070384@gmail.com>
2022-10-15 09:08:01 +0700

    Update hinh anh gallery

--- iodev-backoffice-web/src/components/cms/CuocThi.vue
+++ iodev-backoffice-web/src/components/cms/CuocThi.vue
@@ -200,37 +200,38 @@
             <v-dialog 
               v-model="dialogHinhAnhCuocThi"
               max-width="640px">
-              <v-container>
-                <v-row>
-                  <v-col
-                    v-for="n in 9"
-                    :key="n"
-                    class="d-flex child-flex"
-                    cols="4"
-                  >
-                    <v-img
-                      :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                      :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-                      aspect-ratio="1"
-                      class="grey lighten-2"
+              <v-card>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      v-for="n in 9"
+                      :key="n"
+                      class="d-flex child-flex"
+                      cols="4"
                     >
-                      <template v-slot:placeholder>
-                        <v-row
-                          class="fill-height ma-0"
-                          align="center"
-                          justify="center"
-                        >
-                          <v-progress-circular
-                            indeterminate
-                            color="grey lighten-5"
-                          ></v-progress-circular>
-                        </v-row>
-                      </template>
-                    </v-img>
-                  </v-col>
-                </v-row>
-                <v-card>
-                  <v-card-text style="height: 100px; position: relative">
+                      <v-img
+                        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                        aspect-ratio="1"
+                        class="grey lighten-2"
+                      >
+                        <template v-slot:placeholder>
+                          <v-row
+                            class="fill-height ma-0"
+                            align="center"
+                            justify="center"
+                          >
+                            <v-progress-circular
+                              indeterminate
+                              color="grey lighten-5"
+                            ></v-progress-circular>
+                          </v-row>
+                        </template>
+                      </v-img>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-card-text style="height: 100px; position: relative">
                     <v-fab-transition>
                       <v-btn
                         v-show="true"
@@ -245,8 +246,14 @@
                           </v-btn>
                       </v-fab-transition>
                   </v-card-text>
+
+                  </v-row>
+                </v-container>
+
                 </v-card>
-              </v-container>
+                <v-card-text>
+                  
+                </v-card-text>
               <v-card>
                 <v-card-actions>
                 <v-spacer></v-spacer>
